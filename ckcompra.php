99b908ed11d2d94ba580aa05c297a14058fa367c
Dev-Rodrigo-Marins <digo.marins@hotmail.com>
2023-10-25 17:22:21 -0300

    att compra

--- ckcompra.php
+++ ckcompra.php
@@ -3,30 +3,28 @@
 require_once("./banco.php");
 include_once "./inclusoes/cabecalho.php";
 
-
-
-
-// Execute a Consulta 2 para obter a lista de ativos para compra geral e armazene em $ativos (um array)
-// Suponha que você já tenha as duas consultas prontas e tenha obtido os resultados.
-
-
-
-
 ?>
+
+<script src="validaform.js"></script>
 
 <div class="titulo">
     <h4>Recomendação de investimentos para <?php echo $_SESSION['nome']; ?></h4>
 </div>
 
+<div class="container">
+<button onclick="add_recomendacao()">Usar Recomendações  </button>
+<button onclick="rem_recomendacao()">Remover Recomendações  </button>
+
+</div>
+
 <div class='tabela3'>
-<form action="processar_compra.php" method="post">
-   
+<form action="processar_compra.php" method="get">
     <div class="lista-recomendacoes">
         <h3>Recomendações de Compra:</h3>
         <?php
             // Execute a Consulta 1 para obter as recomendações de compra e armazene em $recomendacoes (um array)
             // Corrija a consulta SQL usando marcadores de posição '?'
-            $sql = "SELECT sg_acao, nm_acao,tb_acao.ds_perfil FROM tb_acao, tb_compra WHERE tb_acao.ds_perfil = ? AND tb_compra.id_usuario = ?";
+            $sql = "SELECT sg_acao, nm_acao,vl_acao,tb_acao.ds_perfil FROM tb_acao, tb_compra WHERE tb_acao.ds_perfil = ? AND tb_compra.id_usuario = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(1, $_SESSION['perfil']);
             $stmt->bindParam(2, $_SESSION['id_usuario']);
@@ -34,15 +32,19 @@ include_once "./inclusoes/cabecalho.php";
 
             $recomendacao =$stmt->fetch(PDO::FETCH_ASSOC);
 
-
-
+            $a=25;    
+            $b=3;
 
 
               while ($recomendacao = $stmt->fetch(PDO::FETCH_ASSOC)) {
                   echo '<label>
-                          <input type="checkbox" name="comprar[]" value="' . $recomendacao['sg_acao'] . '">
-                          ' . $recomendacao['sg_acao'] . '
-                      </label><br>';
+                          <input type="checkbox" name="comprar" id="rec" value="' . $recomendacao['sg_acao'] . '"checked >
+                          ' . $recomendacao['sg_acao'] .'R$'.$recomendacao['vl_acao'].'
+                      </label>
+                      <input type="number" name="quantidade" value="'.($a + (3*$b)).'" placeholder="Digite a quantidade" >
+                      <input type="number" name="total" value="'.($a + (3*$b))*$recomendacao['vl_acao'].'">
+                      <br>';
+                      $b--;
               }
              ?>
     
@@ -51,22 +53,28 @@ include_once "./inclusoes/cabecalho.php";
     <div class="lista-ativos">
         <h3>Ativos para Compra Geral:</h3>
         <?php
-            $sql = "SELECT sg_acao FROM tb_acao where ds_perfil !=?";
+            $sql = "SELECT sg_acao,vl_acao FROM tb_acao where ds_perfil !=?";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(1, $_SESSION['perfil']);
             $stmt->execute();
             $ativos = $stmt->fetch(PDO::FETCH_ASSOC);
 
+            $a=0;    
+            $b=0;
+
             while ($ativos = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 echo '<label>
                         <input type="checkbox" name="comprar[]" value="' . $ativos['sg_acao'] . '">
-                        ' . $ativos['sg_acao'] . '
-                    </label><br>';
+                        ' . $ativos['sg_acao'] .'R$'.$ativos['vl_acao'].'
+                    </label>
+                    <input type="number" name="quantidade" placeholder="Ativos fora do seu perfil de risco!" >
+                    <input type="number" name="total" value="'.($a + (3*$b))*$ativos['vl_acao'].'">
+                      <br>';
             }
         ?>
     </div>
-     
-    <input type="submit" value="Confirmar Compras">
+     <br><br>
+    <input style="text-align: center;" type="submit" value="Confirmar Compras">
 </form>
 </div>
 <?php
